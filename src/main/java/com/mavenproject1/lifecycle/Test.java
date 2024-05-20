4005a31da9ce3152fa95c58063de6fb337cb9d9e
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-05-20 08:14:32 +0530

    bean lifecycle methods using Annotations

--- src/main/java/com/mavenproject1/lifecycle/Test.java
+++ src/main/java/com/mavenproject1/lifecycle/Test.java
@@ -12,12 +12,15 @@ public class Test {
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/mavenproject1/lifecycle/lifecycle_config.xml");
 		context.registerShutdownHook(); // use to destroy method (to destroy method)
 
-		Samosa s1 = (Samosa) context.getBean("s1");
-		System.out.println(s1);
-
-		System.out.println("  ------------------------------  ");
+//		Samosa s1 = (Samosa) context.getBean("s1");
+//		System.out.println(s1);
+//
+//		System.out.println("  ------------------------------  ");
+//		
+//		Pepsi p1 = (Pepsi)context.getBean("p1"); 
+//		System.out.println(p1);
 		
-		Pepsi p1 = (Pepsi)context.getBean("p1"); 
-		System.out.println(p1);
+		Example example = (Example)context.getBean("example");
+		System.out.println(example);
 	}
 }
