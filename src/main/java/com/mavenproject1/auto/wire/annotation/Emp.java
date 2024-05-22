714f87c9aebb0a95155fc4426fb1fe26c4798a01
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-05-22 09:37:53 +0530

    Qualifier Annotation in Autowiring

--- src/main/java/com/mavenproject1/auto/wire/annotation/Emp.java
+++ src/main/java/com/mavenproject1/auto/wire/annotation/Emp.java
@@ -1,11 +1,14 @@
 package com.mavenproject1.auto.wire.annotation;
 
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier; 
 
 public class Emp {
 	// If it is placed above the setter function, setter injection takes place. If placed above the constructor, the constructor injection takes place.
 	// Autowired is placed at either variable, or setter function or constructor
+	
 	@Autowired
+	@Qualifier("address2")
 	private Address address;
 
 	public Address getAddress() {
